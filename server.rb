442f790ca77cba5c04110ec1120544f82120361c
Daniel Morgan <me@danielmorgan.co.uk>
2017-10-11 22:18:55 +0100

    Simplfied to get to start of tutorial

--- server.rb
+++ server.rb
@@ -3,29 +3,12 @@
 require 'socket'
 
 def main
-    socket = Socket.new(Socket::AF_INET, Socket::SOCK_STREAM)
-    addr = Socket.pack_sockaddr_in(9012, '0.0.0.0')
-    socket.setsockopt(Socket::SOL_SOCKET, Socket::SO_REUSEADDR, 1)
-    socket.bind(addr)
+    socket = Socket.new(:INET, :STREAM)
+    socket.setsockopt(Socket::SOL_SOCKET, Socket::SO_REUSEADDR, true)
+    socket.bind(Addrinfo.tcp("127.0.0.1", 9000))
     socket.listen(1)
-
-    loop do
-        connection, addr_info = socket.accept
-        request = connection.recv(4096)
-
-        request.each_line do |line|
-            if line.include? 'foo='
-                key, _, value = line.partition('=')
-                STDOUT.puts "#{key}: #{value}"
-            end
-        end
-
-        content = "Hello world";
-        response = "HTTP/1.1 200 OK\r\nContent-Length: #{content.length}\r\n\r\n#{content}"
-
-        connection.send(response, 0)
-        connection.close
-    end
+    conn_sock, addr_info = socket.accept
+    puts conn_sock.recv(4096)
 end
 
 main
